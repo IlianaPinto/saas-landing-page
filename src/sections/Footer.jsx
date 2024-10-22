bbac45fc9813a800a95b9edaf79e4dd5a14e3827
IlianaPinto <iliana.pinto04@gmail.com>
2024-10-21 18:51:15 -0600

    removed cr

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,9 +5,6 @@ const Footer = () => {
     <footer>
       <div className="container py-10">
         <div className="flex w-full max-md:flex-col">
-          <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
-            <p className="opacity-70">Copyright, JS Mastery</p>
-          </div>
           <div className="flex items-center justify-center sm:ml-auto">
             <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
               Privacy Policy
