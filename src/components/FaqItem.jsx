e240442797ec846c0a021bb175a2f4f12cd8dcf1
IlianaPinto <iliana.pinto04@gmail.com>
2024-10-21 20:18:23 -0600

    removed react scroll

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { useState } from "react";
-import { SlideDown } from "react-slidedown";
-import "react-slidedown/lib/slidedown.css";
+// import { SlideDown } from "react-slidedown";
+// import "react-slidedown/lib/slidedown.css";
 
 // eslint-disable-next-line react/prop-types
 const FaqItem = ({ item, index }) => {
@@ -24,7 +24,7 @@ const FaqItem = ({ item, index }) => {
           <div
             className={clsx(
               "h6 text-p4 transition-colors duration-500 max-md:min-h-20 max-md:items-center",
-              active && "max-lg:text-p1",
+              active && "max-lg:text-p1"
             )}
           >
             {item.question}
@@ -35,21 +35,21 @@ const FaqItem = ({ item, index }) => {
           className={clsx(
             "faq-icon relative flex size-12 items-center justify-center rounded-full border-2 " +
               "border-s2 shadow-400 transition-all duration-500 group-hover:border-s4",
-            active && "before:bg-p1 after:rotate-0 after:bg-p1",
+            active && "before:bg-p1 after:rotate-0 after:bg-p1"
           )}
         >
           <div className="g4 size-11/12 rounded-full shadow-300" />
         </div>
       </div>
-      <SlideDown>
+      <div>
         {activeId === item.id && (
           <div className="body-3 px-7 py-3.5">{item.answer}</div>
         )}
-      </SlideDown>
+      </div>
       <div
         className={clsx(
           "g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 absolute",
-          active && "opacity-100",
+          active && "opacity-100"
         )}
       >
         <div className="g4 absolute inset-0.5 -z-1 rounded-3xl" />
